a93b9c1ef6cc1fae231767e1cb4bdcb9e1d1198d
adityaXcode <adityavaid1204@gmail.com>
2026-06-16 19:32:12 +0530

    Time: 0 ms (100%), Space: 42.8 MB (40.39%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int helper(int nums[], int start, int end){
+        int n= end-start+1;
+        if(n==1) return nums[start];
+        int dp[]= new int[n];
+        dp[0]= nums[start];
+        dp[1]= Math.max(nums[start], nums[start+1]);
+        for(int i=2; i<n; i++){
+            dp[i]= Math.max((nums[start+i]+dp[i-2]),(dp[i-1]));
+        }
+        return dp[n-1];
+    }
+    public int rob(int[] nums) {
+        int n= nums.length;
+        if(n==1) return nums[0];
+        
+        return Math.max(helper(nums,0,n-2), helper(nums,1,n-1));
+    }
+}
