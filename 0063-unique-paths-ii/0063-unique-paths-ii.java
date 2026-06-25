d2e75babd732935dcdc7caf15387abed893ff984
adityaXcode <adityavaid1204@gmail.com>
2026-06-25 16:17:07 +0530

    Time: 0 ms (100%), Space: 43.6 MB (45.54%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,19 +5,19 @@ class Solution {
         int dp[][]= new int[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(obstacleGrid[i][j]==1){
+                if(obstacleGrid[i][j]==1){ //for checking direct obstacle
                     dp[i][j]=0;
                 }
-                else if(i==0 && j==0){
+                else if(i==0 && j==0){ //for checking on first block
                     dp[i][j]=1;
                 }
-                else if(i==0){
+                else if(i==0){ //for checking on first row, agar hua toh aage nhi jaenge
                     dp[i][j]=dp[i][j-1];
                 }
-                else if(j==0){
+                else if(j==0){ //for checking in first col same as above
                     dp[i][j]=dp[i-1][j];
                 }
-                else{
+                else{ //normal sum of upper+leftblock 
                     dp[i][j]=dp[i-1][j]+dp[i][j-1];
                 }
             }
